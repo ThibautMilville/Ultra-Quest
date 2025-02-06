464d1887e00e12207ca3becc6bd3c4d24d89f87f
ThibautMilville <tmilville.pro@gmail.com>
2025-02-06 20:25:39 +0100

    Config

--- frontend/src/utils/axiosInstanceHelper.ts
+++ frontend/src/utils/axiosInstanceHelper.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiUrl: string = import.meta.env.VITE_API_URL || ''
+const apiUrl: string = import.meta.env.VITE_APP_API_URL || ''
 
 export const apiRequestor = axios.create({
   baseURL: apiUrl,
